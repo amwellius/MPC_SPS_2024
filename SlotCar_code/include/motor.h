e5ad880584559a6ec90ea56b2866eb288a354cde
S_KOSIK <samino21@gmail.com>
2024-09-26 21:43:57 +0200

    add basic motor functions

--- SlotCar_code/include/motor.h
+++ SlotCar_code/include/motor.h
@@ -10,26 +10,44 @@
 
 #include <msp430.h>
 // macros definition
-#define H_BREAK_PORT     P8OUT
-#define H_STATUS_PORT    P8OUT
-#define H_DIRECTION_PORT P8OUT
-#define H_IN_PORT        P8OUT // P8.3/TA0.3
+#define H_BRAKE_PORT     P8OUT  // P8.5/TA1.0
+#define H_STATUS_PORT    P8IN   // P8.4/TA0.4  INPUT PORT
+#define H_DIRECTION_PORT P8OUT  // P8.2/TA0.2
+#define H_IN_PORT        P8OUT  // P8.3/TA0.3
 
 // functions prototypes
-#define H_BREAK_PIN
-#define H_STATUS_PIN
+#define H_BRAKE_PIN     0x20    // pin driven low by the H_BRIDGE in case of Vfault
+#define H_STATUS_PIN    0x10    // status pin = gives fault info
 #define H_DIRECTION_PIN 0x04
 #define H_IN_PIN        0x08
 
-// Motor control
-#define MOTOR_ON() (H_IN_PORT |= H_IN_PIN)
-#define MOTOR_OFF() (H_IN_PORT &= ~H_IN_PIN)
+// H_Bridge control
+// old controls
+//#define MOTOR_ON()      (H_IN_PORT |= H_IN_PIN)     // true = motor is ON
+//#define MOTOR_OFF()     (H_IN_PORT &= ~H_IN_PIN)    // false = motor is OFF
+//
+//#define MOTOR_FW()      (H_DIRECTION_PORT |= H_DIRECTION_PIN)   // motor forward mode, logic 11
+//#define MOTOR_RV()      (H_DIRECTION_PORT &= ~H_DIRECTION_PIN)  // motor reverse mode, logic 01
 
-#define MOTOR_FW() (H_DIRECTION_PORT |= H_DIRECTION_PIN)
-#define MOTOR_RV() (H_DIRECTION_PORT &= ~H_DIRECTION_PIN)
+
+#define H_IN_h          (H_IN_PORT |= H_IN_PIN)     // sets pin to high
+#define H_IN_l          (H_IN_PORT &= ~H_IN_PIN)    // sets pin to low
+
+#define H_BRAKE_h       (H_BRAKE_PORT |= H_BRAKE_PIN)
+#define H_BRAKE_l       (H_BRAKE_PORT &= ~H_BRAKE_PIN)
+
+// These below pins can be operated by PWM control. PWM 50%+ changes forward speed, 50%- is reverse speed
+#define H_DIRECTION_h   (H_DIRECTION_PORT |= H_DIRECTION_PIN)
+#define H_DIRECTION_l   (H_DIRECTION_PORT &= ~H_DIRECTION_PIN)
+
 
 // FUNCTIONS
-void motor_init();
+void motor_init(void);
+void motor_forward(void);
+void motor_reverse(void);
+void motor_brake(void);
+
+
 
 // variables
 
--- SlotCar_code/source/motor.c
+++ SlotCar_code/source/motor.c
@@ -7,6 +7,28 @@
 
 #include "include/motor.h"
 
-void motor_init() {
-    P8DIR |= 0x0C; // P8.2 and P8.3 as output
+void motor_init(void) {
+    P8DIR |=    0x2C; // P8.2, P8.3 and P8.5 as outputs
+    P8DIR &= ~  0x10; //P8.4 as input
+}
+
+// function for motor control forward
+void motor_forward(void) {
+    H_IN_h;
+    H_DIRECTION_h;
+    H_BRAKE_l;
+}
+
+// function for motor control reverse
+void motor_reverse(void) {
+    H_IN_h;
+    H_DIRECTION_l;
+    H_BRAKE_l;
+}
+
+// function for motor control brake
+void motor_brake(void) {
+    H_IN_l;
+    H_DIRECTION_l;
+    H_BRAKE_h;
 }
