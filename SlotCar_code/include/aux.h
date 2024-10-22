0fa736acf6fb4c1c8512597b2d2a376837e09c90
S_KOSIK <samino21@gmail.com>
2024-10-22 14:43:53 +0200

    create aux files
    These .h and .c files will include the main functionality of this project, meaning the logic for speed controlling, BLE data handling etc.

---
+++ SlotCar_code/include/aux.h
@@ -0,0 +1,20 @@
+/*
+ * aux.h
+ *
+ *  Created on: 22/October/2024
+ *      Author: xkosik09
+ */
+
+#ifndef AUX_H_
+#define AUX_H_
+
+// INCLUDES
+#include <msp430.h>
+
+// DEFINITIONS
+
+// VARIABLES
+
+// FUNCTIONS
+
+#endif /* AUX_H_ */
---
+++ SlotCar_code/source/aux.c
@@ -0,0 +1,25 @@
+/*
+ * aux.c
+ *
+ *  Created on: 22/October/2024
+ *      Author: xkosik09
+ */
+
+// INCLUDES
+#include "include/aux.h"
+#include <msp430.h>
+
+// VARIABLES
+
+// DEFINITIONS
+
+// FUNCTIONS
+
+
+
+
+
+
+
+
+// **************************************INTERUPTS************************************** //
